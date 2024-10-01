33a0b8dbe112f00fb138d97719714a75acbb76e7
Annie Xu <xannieu24@gmail.com>
2024-09-30 21:00:12 -0400

    fixed bug

--- app/main.py
+++ app/main.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import engine, get_db
 from app.models import order
-from Order_Microservice.app.routers.order_service import order_router  # Import the order router
+from app.routers.order_service import order_router  # Import the order router
 from fastapi.middleware.cors import CORSMiddleware
 
-# Create database tables
 order.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -18,7 +17,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Include the order router with a prefix and tags
 app.include_router(order_router, prefix="/orders", tags=["orders"])
 
 @app.get("/")
